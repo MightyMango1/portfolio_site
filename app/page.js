591a6d01e452db4960bd515b49e86bdea5aaecbe
Nolan Kosmal <nolan.kosmal@gmail.com>
2024-11-13 14:30:15 -0800

    fixed linting issues

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
   //const [isDarkMode, setIsDarkMode] = useState(true);
 
   return (
-    <div className='bg-gray-900 text-white'>
+    <div className="bg-gray-900 text-white">
       <Head>
         <title>Nolan Kosmal - Portfolio</title>
         <meta name="description" content="Personal portfolio website of Nolan Kosmal" />
@@ -42,13 +42,13 @@ export default function Home() {
 
       {/* Home Page Section */}
       <section id ="top" className="flex flex-col items-center justify-center min-h-screen px-4">
-        <Image src="/pfp.jpeg" width={400} height={400} alt='profile picture' className='pb-10'></Image>
+        <Image src="/pfp.jpeg" width={400} height={400} alt="profile picture" className="pb-10"></Image>
         <h1 className="text-5xl font-bold mb-4">Welcome to My Portfolio!</h1>
         <p className="text-xl text-gray-400 max-w-2xl text-center">
           I am Nolan Kosmal, a computer science with business applications student passionate about building impactful projects. 
           Explore my journey, see my work, and learn more about my interests.
         </p>
-        <div className='flex justify-center items-center p-12'>
+        <div className="flex justify-center items-center p-12">
         <ul className="flex-1 flex justify-center space-x-6">
           <li><a href="https://www.linkedin.com/in/nolan-kosmal/" target="_blank" className="hover:text-gray-400">Linkedin</a></li>
           <li><a href="https://www.github.com/MightyMango1" target="_blank" className="hover:text-gray-400">Github</a></li>
@@ -61,13 +61,13 @@ export default function Home() {
       <section id="about" className="p-12">
         <h2 className="text-3xl font-bold mb-4">About Me</h2>
         <p className="text-lg text-gray-400">
-          Hi! I'm Nolan. I am a student at the University of California, Riverside, pursuing a Bachelor of Science in Computer Science with Business Applications. I've been coding
-          since the early years of high school. I've built a multitude of projects in the past couple years and I'm currently playing around with Web Design and some backend frameworks/languages. It can be 
+          Hi! I{'\'m'} Nolan. I am a student at the University of California, Riverside, pursuing a Bachelor of Science in Computer Science with Business Applications. I{'\'v'}e been coding
+          since the early years of high school. I{'\'v'}e built a multitude of projects in the past couple years and I{'\'m'} currently playing around with Web Design and some backend frameworks/languages. It can be 
           challenging learning things on your own, but it can also be fun because I am constantly learning new things!
           <br/>
           <br/>
            My entire life has been deeply shaped by a love for adventure, creativity, and learning. My main interests revolve around the outdoors, especially in rock climbing.
-           I've been passionate about climbing for over 10 years. It’s a passion that’s taken me to some amazing places, and it’s something I’m always excited to keep pursuing. 
+           I{'\'v'}e been passionate about climbing for over 10 years. It{'\'s'} a passion that{'\'s'} taken me to some amazing places, and it{'\'s'} something I{'\'m'} always excited to keep pursuing. 
            <br/>
            <br/>
           I also have a strong passion for vintage clothing. A few years ago, I combined my love for fashion and nostalgia by founding a vintage clothing store on Depop. 
@@ -89,7 +89,7 @@ export default function Home() {
           </div>
           <div className="p-6 bg-gray-800 rounded-lg shadow-md hover:scale-105 transition-transform">
             <h3 className="text-xl font-semibold mb-2">World Guesser Bot</h3>
-            <p className="text-gray-400">Designed and implemented an interactive solving bot that suggests optimal guesses for the game "Worlde".</p>
+            <p className="text-gray-400">Designed and implemented an interactive solving bot that suggests optimal guesses for the game {'\"'}Worlde{'\"'}.</p>
           </div>
           <div className="p-6 bg-gray-800 rounded-lg shadow-md hover:scale-105 transition-transform">
             <h3 className="text-xl font-semibold mb-2">Pet Activity Monitor</h3>
@@ -116,7 +116,7 @@ export default function Home() {
       {/* Footer */}
       <footer className="text-center py-4 border-t border-gray-700">
         <p>© 2024 Nolan Kosmal. All rights reserved.</p>
-        <a href="#top" className='text-gray-400'>Back to the top</a>
+        <a href="#top" className="text-gray-400">Back to the top</a>
       </footer>
     </div>
   );
