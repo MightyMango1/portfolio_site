bd390f5d60bbf43b7c9937f5107a7bda6942f2b8
Nolan Kosmal <nolan.kosmal@gmail.com>
2024-11-13 21:41:25 -0800

    updated ABOUT section of site

--- app/page.js
+++ app/page.js
@@ -42,7 +42,7 @@ export default function Home() {
 
       {/* Home Page Section */}
       <section id ="top" className="flex flex-col items-center justify-center min-h-screen px-4">
-        <Image src="./pfp.jpeg" width={400} height={400} alt="profile picture" className="pb-10"></Image>
+        <Image src="./pfp.jpeg" width={400} height={400} alt="profile picture" className="pb-10 hover:scale-105 transform transition duration-30 transition-transform"></Image>
         <h1 className="text-5xl font-bold mb-4">Welcome to My Portfolio!</h1>
         <p className="text-xl text-gray-400 max-w-2xl text-center">
           I am Nolan Kosmal, a computer science with business applications student passionate about building impactful projects. 
@@ -60,45 +60,72 @@ export default function Home() {
       {/* About Section */}
       <section id="about" className="p-12">
         <h2 className="text-3xl font-bold mb-4">About Me</h2>
-        <p className="text-lg text-gray-400">
+        <div className="text-lg text-gray-400">
+        <p>
           Hi! I{'\'m'} Nolan. I am a student at the University of California, Riverside, pursuing a Bachelor of Science in Computer Science with Business Applications. I{'\'v'}e been coding
           since the early years of high school. I{'\'v'}e built a multitude of projects in the past couple years and I{'\'m'} currently playing around with Web Design and some backend frameworks/languages. It can be 
           challenging learning things on your own, but it can also be fun because I am constantly learning new things!
-          <br/>
-          <br/>
-           My entire life has been deeply shaped by a love for adventure, creativity, and learning. My main interests revolve around the outdoors, especially in rock climbing.
-           I{'\'v'}e been passionate about climbing for over 10 years. It{'\'s'} a passion that{'\'s'} taken me to some amazing places, and it{'\'s'} something I{'\'m'} always excited to keep pursuing. 
-           <br/>
-           <br/>
-          I also have a strong passion for vintage clothing. A few years ago, I combined my love for fashion and nostalgia by founding a vintage clothing store on Depop. 
+        </p>
+        <p className ="py-3">
+            My entire life has been deeply shaped by a love for adventure, creativity, and learning. My main interests revolve around the outdoors, especially in rock climbing.
+            I{'\'v'}e been passionate about climbing for over 10 years. It{'\'s'} a passion that{'\'s'} taken me to some amazing places, and it{'\'s'} something I{'\'m'} always excited to keep pursuing. 
+        </p>
+        <div className="grid grid-cols-3 md:grid-cols-3 gap-6 p-6 bg-gray-800 rounded-lg shadow-md">
+        <figure className="overflow-hidden rounded-lg shadow-lg transform transition duration-300 hover:brightness-110 hover:shadow-2xl">
+          <Image src="./camping.jpeg" width={250} height={150} alt="camping picture" className="rounded-t-lg"/>
+          <figcaption className="text-gray-300 mt-2 p-2 bg-gray-900 rounded-b-lg  text-sm">Backpacking in the mountains of Bishop, CA</figcaption>
+        </figure>
+        <figure className="overflow-hidden rounded-lg shadow-lg transform transition duration-300 hover:brightness-110 hover:shadow-2xl">
+          <Image src="./climbing1.jpeg" width={250} height={150} alt="climbing picture" className="rounded-t-lg"/>
+          <figcaption className="text-gray-300 mt-2 p-2 bg-gray-900 rounded-b-lg  text-sm">Bouldering at Black Mtn. with Friends</figcaption>
+        </figure>
+        <figure className="overflow-hidden rounded-lg shadow-lg transform transition duration-300 hover:brightness-110 hover:shadow-2xl">
+          <Image src="./climbing2.jpeg" width={250} height={150} alt="climbing picture" className="rounded-t-lg"/>
+          <figcaption className="text-gray-300 mt-2 p-2 bg-gray-900 rounded-b-lg  text-sm">Deep water soloing at Stoddard Falls</figcaption>
+        </figure>
+      </div>
+        
+        <p className = "pt-6">
+          I also have a strong passion for vintage clothing and retro electronics. A few years ago, I combined my love for fashion and nostalgia by founding a vintage clothing store on Depop. 
           Curating and offering timeless pieces to others has been an exciting creative outlet, allowing me to share my appreciation for unique, classic style. Feel free to check it out!
         </p>
+        </div>
+
+  <div className="mt-4 text-center pt-4">
+    <a href="https://www.depop.com/kozbrosthrift/?brandIds=486&productId=449521483" target="_blank" className="inline-block px-6 py-3 text-lg font-semibold text-white bg-gray-700 hover:bg-gray-600 rounded-md shadow-md transition duration-300">Visit My Depop Shop</a>
+  </div>
+
       </section>
 
       {/* Projects Section */}
       <section id="projects" className="p-12">
         <h2 className="text-3xl font-bold mb-4">Projects</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          <div className="p-6 bg-gray-800 rounded-lg shadow-md hover:scale-105 transition-transform">
+          <div className="p-6 bg-gray-800 rounded-lg shadow-md hover:scale-105 hover:shadow-2xl hover:brightness-110 transition-transform">
             <h3 className="text-xl font-semibold mb-2">Quizlet-Style Study App</h3>
             <p className="text-gray-400">Led a team of 7+ developers to create an educational study application for students.</p>
           </div>
-          <div className="p-6 bg-gray-800 rounded-lg shadow-md hover:scale-105 transition-transform">
+          <div className="p-6 bg-gray-800 rounded-lg shadow-md hover:scale-105 hover:shadow-2xl hover:brightness-110 transition-transform">
             <h3 className="text-xl font-semibold mb-2">AI Prompt Generator</h3>
             <p className="text-gray-400">Built an AI-driven tool for generating optimal prompts for text generation models.</p>
           </div>
-          <div className="p-6 bg-gray-800 rounded-lg shadow-md hover:scale-105 transition-transform">
+          <div className="p-6 bg-gray-800 rounded-lg shadow-md hover:scale-105 hover:shadow-2xl hover:brightness-110 transition-transform">
             <h3 className="text-xl font-semibold mb-2">World Guesser Bot</h3>
             <p className="text-gray-400">Designed and implemented an interactive solving bot that suggests optimal guesses for the game {'\"'}Worlde{'\"'}.</p>
           </div>
-          <div className="p-6 bg-gray-800 rounded-lg shadow-md hover:scale-105 transition-transform">
+          <div className="p-6 bg-gray-800 rounded-lg shadow-md hover:scale-105 hover:shadow-2xl hover:brightness-110 transition-transform">
             <h3 className="text-xl font-semibold mb-2">Pet Activity Monitor</h3>
             <p className="text-gray-400">Developed a pet activity tracker, allowing pet owners to easily log and monitor pet meals in real-time.</p>
           </div>
-          <div className="p-6 bg-gray-800 rounded-lg shadow-md hover:scale-105 transition-transform">
+          <div className="p-6 bg-gray-800 rounded-lg shadow-md hover:scale-105 hover:shadow-2xl hover:brightness-110 transition-transform">
             <h3 className="text-xl font-semibold mb-2">2D Sandbox Survival Game</h3>
             <p className="text-gray-400">Built a cooperative sandbox survival game where players explore, mine, craft, and battle in a procedurally generated underground world. Built using the 
               Godot game engine </p>
+          </div>
+          <div className="p-6 bg-gray-800 rounded-lg shadow-md hover:scale-105 hover:shadow-2xl hover:brightness-110 transition-transform">
+            <h3 className="text-xl font-semibold mb-2">This Website!</h3>
+            <p className="text-gray-400">This portfolio webiste was built with Next.js and deployed via Github Pages. While it may be small {'(but mighty)'}, its the perfect
+              place to showcase my hobbies and accomplishments. Everything is up to date {'(as of 11/24)'}</p>
           </div>
         </div>
       </section>
